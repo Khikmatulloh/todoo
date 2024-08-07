fb95a98b48ac761fec5397959f14ab95150b78e3
Xikmatulloh <hikmatmahmadjonov20060826.@gmail.com>
2024-08-07 07:39:43 +0300

    change

--- view/partials/navbar.php
+++ view/partials/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container">
+<div class="container">
         <a class="navbar-brand" href="/">TODO App</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -9,15 +9,33 @@
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/todos">Todo list</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/notes">Notes</a>
-                </li>
+                <?php if (isset($_SESSION['user'])): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/todos">Todo list</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/notes">Notes</a>
+                    </li>
+                <?php endif; ?>
             </ul>
-            <a href="/login" class="btn btn-outline-primary mx-2">Login</a>
-            <a href="/register" class="btn btn-outline-success">Register</a>
+            <ul class="navbar-nav ml-auto">
+                <?php if (!isset($_SESSION['user'])): ?>
+                    <li class="nav-item">
+                        <a class="btn btn-outline-primary mx-2" href="/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-outline-success" href="/register">Register</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="btn btn-outline-danger mx-2" href="/logout">Logout</a>
+                    </li>
+                    <?php
+                    $id = rand(1000000000, 9999999999);
+                    echo "<a href='https://t.me/mt_todo_app_bot?start=$id' class='ms-2 text-underline'>Connect to telegram</a>";
+                    ?>
+                <?php endif; ?>
+            </ul>
         </div>
     </div>
 </nav>
